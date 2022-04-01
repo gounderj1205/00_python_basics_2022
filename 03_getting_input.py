27bd05cc53e019de85e3430276e67307c56bda33
gounderj1205 <101751162+gounderj1205@users.noreply.github.com>
2022-04-01 15:00:08 +1300

    I was learning about input

---
+++ 03_getting_input.py
@@ -0,0 +1,25 @@
+# Ask the user for their name
+username = input("what is your name? ")
+
+# Ask the user for their favourite integer
+fav_num = int("favourite number? ")
+
+# Double, halve and square the number
+double = fav_num * 7
+half = fav_num / 7
+squared = fav_num * fav_num
+
+print()
+# greet the user
+print("Hi {}, your favourite number is {}".format(username, fav_num))
+print()
+# Output the results of doubling, halving
+# and squaring their favourite number
+print()
+print
+print
+print
+
+
+
+
